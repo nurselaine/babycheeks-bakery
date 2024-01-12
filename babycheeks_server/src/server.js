d0be2d8bf4c7183ce905d6237b8256531798ef3b
nurselaine <Elaine.t.huynh@gmail.com>
2024-01-11 16:22:50 -0800

    Connected simple express server with contentful CMS

--- babycheeks_server/src/server.js
+++ babycheeks_server/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const contentful = require('contentful');
 
 const app = express();
+require('dotenv').config();
+
 const PORT = 3001 || 3002;
 
 const logger = require("./middlewares/logger");
@@ -20,6 +23,21 @@ app.use("/request-type", (req, res, next) => {
 app.get("/", (req, res) => {
   console.log("Hello Baby Cheeks Server");
 })
+
+const client = contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE,
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: process.env.CONTENTFUL_ACCESSTOKEN
+  })
+
+  client.getEntries().then((entries) => {
+    entries.items.forEach((entry) => {
+        if(entry.fields.title) {
+            console.log(entry.fields.title);
+            console.log(entry.fields.description);
+        }
+    })
+  })
 
 const start = () => {
     app.listen(PORT, () => { console.log(`Listening on PORT ${PORT}`)});
