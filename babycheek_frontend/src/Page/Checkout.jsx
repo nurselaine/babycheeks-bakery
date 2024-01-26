5266c0339e090422889aa14850bdb27017b49530
nurselaine <Elaine.t.huynh@gmail.com>
2024-01-26 01:37:02 -0800

    Transaction pane and checkout page complete

--- babycheek_frontend/src/Page/Checkout.jsx
+++ babycheek_frontend/src/Page/Checkout.jsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import LandingLayout from "../Layout/LandingLayout";
 import TransactionPane from "../component/transaction/TransactionPane";
+import data from '../utils/home_data.json';
 import './Page.css';
 
-let cookies = [0, 1, 2, 3, 4, 5];
-
 const Checkout = () => {
+  const cookies= [0, 1, 2, 3, 4, 5];
+  const [cost] = useState([5.4, 4.75, 8.99, 4.45, 5, 6.54]);
   return (
     <LandingLayout>
       <div className="checkout-page">
         <div className="store-items">
           {
-            cookies.map((c) => (
+            cookies.map((item, idx) => (
               <TransactionPane 
-                item_img={'./assets/item/cookie_butter_cookie.png'}
-                title={'cookie butter cookie'}
-                desc={'so ymmy and delicious yumm lala mummsidnsja.'} 
-                cost={5}
+                item_img={data.cookie[idx]}
+                title={data.cookie_title[idx]}
+                desc={data.cookie_description[idx]}
+                cost={cost[idx]}
+                key={`${idx}`}
               />
             ))
           }
