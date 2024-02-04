7334364e70ef698e342d7ef1e533d1cb51731664
nurselaine <Elaine.t.huynh@gmail.com>
2024-02-04 15:25:58 -0800

    Added routes for all CMS infomation

--- babycheeks_server/src/routes/menu.js
+++ babycheeks_server/src/routes/menu.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const client = require("../services/cf");
 
 // get all CMS entires route
-router.get("/", async (req, res, next) => {
+router.get("/title", async (req, res, next) => {
     try {
         let cookies = [];
         let entries = await client.getEntries();
@@ -14,8 +14,6 @@ router.get("/", async (req, res, next) => {
                 cookies.push(entry.fields.title);
             }
         });
-
-        console.log(cookies);
 
         res.status(200).send({
             items: cookies,
@@ -30,5 +28,54 @@ router.get("/", async (req, res, next) => {
         })
     }
 });
+
+router.get('/description', async (req, res, next) => {
+    try {
+        let desc = [];
+        let entries = await client.getEntries();
+        entries.items.forEach((entry) => {
+            console.log(entry.fields.brandname);
+            if (entry.fields.description) {
+                desc.push(entry.fields.description);
+            }
+        });
+
+        res.status(200).send({
+            items: desc,
+            message: "Cookies retrieved successfully.",
+            timestamp: new Date(),
+        });
+    } catch (error) {
+        console.error('Error requesting all menu items', error);
+        res.status(400).send({
+            message: "internal server error",
+            timestamp: new Date(),
+        })
+    }
+})
+
+router.get('/brand', async (req, res, next) => {
+    try {
+        let brandname = "";
+        let entries = await client.getEntries();
+        entries.items.forEach((entry) => {
+            if (entry.fields.brandname) {
+                brandname = entry.fields.brandname;
+            }
+        });
+
+        res.status(200).send({
+            items: brandname,
+            message: "Cookies retrieved successfully.",
+            timestamp: new Date(),
+        });
+    } catch (error) {
+        console.error('Error requesting all menu items', error);
+        res.status(400).send({
+            message: "internal server error",
+            timestamp: new Date(),
+        })
+    }
+})
 
 module.exports = router;
